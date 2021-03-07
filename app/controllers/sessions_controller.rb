e9497143b5f96b4b58722f774726eb77b0865f52
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-06 21:09:55 -0500

    added sign in form and controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,19 @@ class SessionsController < ApplicationController
   def new 
   end
 
-  def create 
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
   end
 
-  def destroy 
+  def destroy
+    session[:user_id] = nil
+    redirect_to signin_path, notice: "Logged out"
   end
 end
