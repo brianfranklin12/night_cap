2e21ea7b20c31b3abe7fd0272661c2934e320d83
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-06 20:43:32 -0500

    created new user form and controller logic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,16 @@
 class RegistrationsController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def create 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new
+    end
   end
 
   private
@@ -10,5 +18,5 @@ class RegistrationsController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
-  
+
 end
