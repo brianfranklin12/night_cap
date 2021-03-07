73a828d91cd768db3b15e00974f116e9ede6bb7d
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-06 22:00:55 -0500

    added the cocktail partial to the index view

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,6 @@
 class CocktailsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
   
   def index
     @cocktails = current_user.cocktails
@@ -16,6 +17,32 @@ class CocktailsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktails_path, notice: "Cocktail was updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path, notice: "Cocktail was removed from your collection"
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :style)
+  end
+
+  def set_cocktail 
+    @cocktail = current_user.cocktails.find(params[:id])
   end
 
 
