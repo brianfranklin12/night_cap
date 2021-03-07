ab9d94562fa53ff1e8b6449b84a094af87326f8d
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-06 21:01:14 -0500

    created sessions controller /routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get "/signup" => "registrations#new"
   post "/signup" => "registrations#create"
+
+  get "/signin" => "sessions#new"
+  post "/signin" => "sessions#create"
+
+  delete "/signout" => "sessions#destroy"
+  
 end
