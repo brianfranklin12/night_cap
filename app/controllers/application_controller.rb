ff87b02ba8ada886ccbf15332f4c58f85a28c6a8
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-06 20:57:56 -0500

    added current_user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_user_logged_in!
+    redirect_to signin_path, alert: "You must be signed in to do that." if current_user.nil?
+  end
 end
