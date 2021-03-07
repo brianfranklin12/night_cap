27e1fad876267bc2880a5438459a81a5d88952a6
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-06 21:51:46 -0500

    generated new cocktail form and controller logic, including form partial

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,4 +4,19 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = current_user.cocktails
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = current_user.cocktails.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path, notice: "Cocktail was added to your collection"
+    else
+      render :new
+    end
+  end
+
+
 end
