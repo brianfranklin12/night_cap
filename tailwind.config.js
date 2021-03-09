bef97394ecb0740262204eb012dcac5fd8d49a45
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-09 15:54:42 -0500

    tailwind styles to sign in and sign up forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,12 @@ module.exports = {
       colors: {
         greyblue: '#99abb0',
         purple: '#8683a3',
-        red: '#fa7b79',
+        red: {
+          DEFAULT: '#fa7b79',
+          dark: '#e16e6c',
+        },
         yellow: '#ffdfb5',
-        pink: '#fa7b79',
+        pink: '#fcbdbc',
         grey: '#4d4d4d',
       },
     },
