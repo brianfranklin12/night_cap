b08a76ec8c5a305d58d028ef8a170051cb6cd43c
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-09 10:31:30 -0500

    added custom colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greyblue: '#99abb0',
+        purple: '#8683a3',
+        red: '#fa7b79',
+        yellow: '#ffdfb5',
+        pink: '#fa7b79',
+        grey: '#4d4d4d',
+      },
+    },
   },
   variants: {
     extend: {},
