b0b8fa04c273116f5aa7a6762647aabbcac809b5
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-10 00:44:00 -0500

    added description to cocktails model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_024306) do
+ActiveRecord::Schema.define(version: 2021_03_10_035512) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_024306) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["user_id"], name: "index_cocktails_on_user_id"
   end
 
