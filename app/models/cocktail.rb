7d8e544cd5f1620fb2a9bd51d2fdcb45356db16f
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-17 14:45:16 -0400

    built in the functionality of added/removing ingredients in cocktail form

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,7 +4,7 @@ class Cocktail < ApplicationRecord
   has_many :cocktail_ingredients, dependent: :destroy
   has_many :ingredients, through: :cocktail_ingredients
   
-  accepts_nested_attributes_for :cocktail_ingredients
+  accepts_nested_attributes_for :cocktail_ingredients, allow_destroy: true
 
   validates_presence_of :name, :style, :description
 
