dbcac323ed46fb9fdcead278c2049df5cc3db548
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-19 15:35:02 -0400

    added find_user_ingredients and used it to list user's ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,20 @@
 class IngredientsController < ApplicationController
+  before_action :require_user_logged_in!
+  before_action :find_user_ingredients
+
   def index
-    @ingredients = current_user.ingredients
   end
+
+  private 
+
+    def find_user_ingredients
+      @ingredients = []
+      current_user.cocktails.each do |cocktail|
+        cocktail.ingredients.each do |ingredient|
+          @ingredients << ingredient if !@ingredients.include? ingredient 
+        end
+      end
+      @ingredients
+    end
+    
 end
