ea6a65b51e8dfdcdc55950fa03747f8e8bdc5cf5
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-22 19:48:29 -0400

    changed ingredients to show all ingredients rather than user's

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,25 +1,15 @@
 class IngredientsController < ApplicationController
   before_action :require_user_logged_in!
-  before_action :find_user_ingredients
   before_action :set_ingredient, only: :show
 
   def index
+    @ingredients = Ingredient.all
   end
 
   private 
 
-    def find_user_ingredients
-      @ingredients = []
-      current_user.cocktails.each do |cocktail|
-        cocktail.ingredients.each do |ingredient|
-          @ingredients << ingredient if !@ingredients.include? ingredient 
-        end
-      end
-      @ingredients
-    end
-
     def set_ingredient
-      @ingredient = Ingredient.find(params[:id]) if @ingredients.include? Ingredient.find(params[:id])
+      @ingredient = Ingredient.find(params[:id])
     end
     
 end
