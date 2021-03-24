0d326e549cc28054558f167379853625c690376f
Brian Franklin <brianfranklinmusic@gmail.com>
2021-03-24 10:57:02 -0400

    style changes for comments

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,5 @@
 class Ingredient < ApplicationRecord
   has_many :cocktail_ingredients, dependent: :destroy
   has_many :cocktails, through: :cocktail_ingredients
-  has_many :likes, dependent: :destroy
 
 end
